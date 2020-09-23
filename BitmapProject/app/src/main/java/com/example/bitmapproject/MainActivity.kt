3090b1b6c0b108fc972ed2f1240e20969f1e3612
DoanNguyenNamHong <hongdnnse130287@fpt.edu.vn>
2020-09-23 14:32:38 +0700

    create bitmap project

---
+++ BitmapProject/app/src/main/java/com/example/bitmapproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bitmapproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
